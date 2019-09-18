8176135/druid

Update druid-shell menus API

druid-shell/src/mac/menu.rs
@@ -13,14 +13,15 @@
 // limitations under the License.
 
 //! macOS implementation of menus.
-use cocoa::appkit::{NSMenu, NSMenuItem};
+use cocoa::appkit::{NSEventModifierFlags, NSMenu, NSMenuItem};
 use cocoa::base::{id, nil};
 use cocoa::foundation::NSAutoreleasePool;
 use lazy_static::lazy_static;
 use objc::declare::ClassDecl;
 use objc::runtime::{Class, Object, Sel};
 
-use crate::keycodes::{KeySpec, MenuKey};
+use crate::hotkey::{HotKey, KeyCompare};
+use crate::keyboard::{KeyCode, KeyModifiers};
 use crate::util::make_nsstring;
 
 struct MenuItemProxyClass(*const Class);
@@ -50,14 +51,13 @@ pub struct Menu {
 
 /// Make a string in the syntax expected as the keyEquivalent argument to
 /// NSMenuItem initWithTitle:action:keyEquivalent:
-fn make_key_equivalent(key: impl Into<MenuKey>) -> String {
-    // TODO: handle modifiers and other fun things
-    let key = key.into();
-    match key.key {
-        KeySpec::Char(c) => c.to_string(),
-        KeySpec::None => "".to_string(),
-    }
-}
+//fn make_key_equivalent(key: &HotKey) -> String {
+//// TODO: handle modifiers and other fun things
+//match key.key {
+//KeySpec::Char(c) => c.to_string(),
+//KeySpec::None => "".to_string(),
+//}
+//}
 
 /// Strip the access keys from the menu strong.
 ///
@@ -79,17 +79,21 @@ fn strip_access_key(raw_menu_text: &str) -> String {
     result
 }
 
-fn make_basic_menu_item(_id: u32, text: &str, key: impl Into<MenuKey>) -> id {
-    let key_equivalent = make_key_equivalent(key);
+fn make_basic_menu_item(_id: u32, text: &str, key: Option<&HotKey>) -> id {
+    let key_equivalent = key.map(HotKey::key_equivalent).unwrap_or("");
     let stripped_text = strip_access_key(text);
     unsafe {
-        NSMenuItem::alloc(nil)
+        let item = NSMenuItem::alloc(nil)
             .initWithTitle_action_keyEquivalent_(
                 make_nsstring(&stripped_text),
                 sel!(trigger),
                 make_nsstring(&key_equivalent),
             )
-            .autorelease()
+            .autorelease();
+        if let Some(mask) = key.map(HotKey::key_modifier_mask) {
+            msg_send![item, setKeyEquivalentModifierMask: mask];
+        }
+        item
     }
 }
 
@@ -101,7 +105,7 @@ fn make_proxy(id: u32) -> id {
     }
 }
 
-fn make_menu_item(id: u32, text: &str, key: impl Into<MenuKey>) -> id {
+fn make_menu_item(id: u32, text: &str, key: Option<&HotKey>) -> id {
     let target = make_proxy(id);
     let menu_item = make_basic_menu_item(id, text, key);
     unsafe {
@@ -120,15 +124,19 @@ impl Menu {
     }
 
     // TODO: how do we use the text here?
-    pub fn add_dropdown(&mut self, menu: Menu, _text: &str) {
+    pub fn add_dropdown(&mut self, menu: Menu, text: &str) {
         unsafe {
-            let menu_item = NSMenuItem::new(nil).autorelease();
+            let menu_item = NSMenuItem::alloc(nil);
+            let title = make_nsstring(text);
+            msg_send![menu_item, setTitle: title];
+            msg_send![menu.menu, setTitle: title];
             menu_item.setSubmenu_(menu.menu);
             self.menu.addItem_(menu_item);
         }
     }
 
-    pub fn add_item(&mut self, id: u32, text: &str, key: impl Into<MenuKey>) {
+    pub fn add_item(&mut self, id: u32, text: &str, key: Option<&HotKey>) {
+        eprintln!("{}: {}", id, text);
         let menu_item = make_menu_item(id, text, key);
         unsafe {
             self.menu.addItem_(menu_item);
@@ -149,8 +157,78 @@ impl Default for Menu {
         let mut menu = Menu::new();
         // this one is our actual menu
         let mut submenu = Menu::new();
-        submenu.add_item(1, "Quit", 'q');
+        //submenu.add_item(1, "Quit", 'q');
         menu.add_dropdown(submenu, "Application");
         menu
+    }
+}
+impl HotKey {
+    /// Return the string value of this hotkey, for use with Cocoa `NSResponder`
+    /// objects.
+    ///
+    /// Returns the empty string if no key equivalent is known.
+    fn key_equivalent(&self) -> &str {
+        match self.key {
+            KeyCompare::Text(t) => t,
+
+            // from NSText.h
+            KeyCompare::Code(KeyCode::Return) => "\u{0003}",
+            KeyCompare::Code(KeyCode::Backspace) => "\u{0008}",
+            KeyCompare::Code(KeyCode::Delete) => "\u{007f}",
+            // from NSEvent.h
+            KeyCompare::Code(KeyCode::Insert) => "\u{F727}",
+            KeyCompare::Code(KeyCode::Home) => "\u{F729}",
+            KeyCompare::Code(KeyCode::End) => "\u{F72B}",
+            KeyCompare::Code(KeyCode::PageUp) => "\u{F72C}",
+            KeyCompare::Code(KeyCode::PageDown) => "\u{F72D}",
+            KeyCompare::Code(KeyCode::PrintScreen) => "\u{F72E}",
+            KeyCompare::Code(KeyCode::ScrollLock) => "\u{F72F}",
+            KeyCompare::Code(KeyCode::ArrowUp) => "\u{F700}",
+            KeyCompare::Code(KeyCode::ArrowDown) => "\u{F701}",
+            KeyCompare::Code(KeyCode::ArrowLeft) => "\u{F702}",
+            KeyCompare::Code(KeyCode::ArrowRight) => "\u{F703}",
+            KeyCompare::Code(KeyCode::F1) => "\u{F704}",
+            KeyCompare::Code(KeyCode::F2) => "\u{F705}",
+            KeyCompare::Code(KeyCode::F3) => "\u{F706}",
+            KeyCompare::Code(KeyCode::F4) => "\u{F707}",
+            KeyCompare::Code(KeyCode::F5) => "\u{F708}",
+            KeyCompare::Code(KeyCode::F6) => "\u{F709}",
+            KeyCompare::Code(KeyCode::F7) => "\u{F70A}",
+            KeyCompare::Code(KeyCode::F8) => "\u{F70B}",
+            KeyCompare::Code(KeyCode::F9) => "\u{F70C}",
+            KeyCompare::Code(KeyCode::F10) => "\u{F70D}",
+            //KeyCompare::Code(KeyCode::F11)            => "\u{F70E}",
+            //KeyCompare::Code(KeyCode::F12)            => "\u{F70F}",
+            //KeyCompare::Code(KeyCode::F13)            => "\u{F710}",
+            //KeyCompare::Code(KeyCode::F14)            => "\u{F711}",
+            //KeyCompare::Code(KeyCode::F15)            => "\u{F712}",
+            //KeyCompare::Code(KeyCode::F16)            => "\u{F713}",
+            //KeyCompare::Code(KeyCode::F17)            => "\u{F714}",
+            //KeyCompare::Code(KeyCode::F18)            => "\u{F715}",
+            //KeyCompare::Code(KeyCode::F19)            => "\u{F716}",
+            //KeyCompare::Code(KeyCode::F20)            => "\u{F717}",
+            KeyCompare::Code(other) => {
+                eprintln!("no key equivalent for {:?}", other);
+                ""
+            }
+        }
+    }
+
+    fn key_modifier_mask(&self) -> NSEventModifierFlags {
+        let mods: KeyModifiers = self.mods.into();
+        let mut flags = NSEventModifierFlags::empty();
+        if mods.shift {
+            flags.insert(NSEventModifierFlags::NSShiftKeyMask);
+        }
+        if mods.meta {
+            flags.insert(NSEventModifierFlags::NSCommandKeyMask);
+        }
+        if mods.alt {
+            flags.insert(NSEventModifierFlags::NSAlternateKeyMask);
+        }
+        if mods.ctrl {
+            flags.insert(NSEventModifierFlags::NSControlKeyMask);
+        }
+        flags
     }
 }

Strong-1107/druid-calculator

druid-shell/src/mac/menu.rs
@@ -13,11 +13,14 @@
 // limitations under the License.
 
 //! macOS implementation of menus.
-use crate::keycodes::{KeySpec, MenuKey};
+
 use crate::util::make_nsstring;
-use cocoa::appkit::{NSMenu, NSMenuItem};
+use cocoa::appkit::{NSEventModifierFlags, NSMenu, NSMenuItem};
 use cocoa::base::{id, nil, NO};
 use cocoa::foundation::NSAutoreleasePool;
+
+use crate::hotkey::{HotKey, KeyCompare};
+use crate::keyboard::{KeyCode, KeyModifiers};
 
 pub struct Menu {
     pub menu: id,
@@ -25,14 +28,13 @@ pub struct Menu {
 
 /// Make a string in the syntax expected as the keyEquivalent argument to
 /// NSMenuItem initWithTitle:action:keyEquivalent:
-fn make_key_equivalent(key: impl Into<MenuKey>) -> String {
-    // TODO: handle modifiers and other fun things
-    let key = key.into();
-    match key.key {
-        KeySpec::Char(c) => c.to_string(),
-        KeySpec::None => "".to_string(),
-    }
-}
+//fn make_key_equivalent(key: &HotKey) -> String {
+//// TODO: handle modifiers and other fun things
+//match key.key {
+//KeySpec::Char(c) => c.to_string(),
+//KeySpec::None => "".to_string(),
+//}
+//}
 
 /// Strip the access keys from the menu strong.
 ///
@@ -54,8 +56,8 @@ fn strip_access_key(raw_menu_text: &str) -> String {
     result
 }
 
-fn make_menu_item(id: u32, text: &str, key: impl Into<MenuKey>) -> id {
-    let key_equivalent = make_key_equivalent(key);
+fn make_menu_item(_id: u32, text: &str, key: Option<&HotKey>) -> id {
+    let key_equivalent = key.map(HotKey::key_equivalent).unwrap_or("");
     let stripped_text = strip_access_key(text);
     unsafe {
         let item = NSMenuItem::alloc(nil)
@@ -65,7 +67,9 @@ fn make_menu_item(id: u32, text: &str, key: impl Into<MenuKey>) -> id {
                 make_nsstring(&key_equivalent),
             )
             .autorelease();
-        msg_send![item, setTag: id as isize];
+        if let Some(mask) = key.map(HotKey::key_modifier_mask) {
+            msg_send![item, setKeyEquivalentModifierMask: mask];
+        }
         item
     }
 }
@@ -80,15 +84,19 @@ impl Menu {
     }
 
     // TODO: how do we use the text here?
-    pub fn add_dropdown(&mut self, menu: Menu, _text: &str) {
+    pub fn add_dropdown(&mut self, menu: Menu, text: &str) {
         unsafe {
-            let menu_item = NSMenuItem::new(nil).autorelease();
+            let menu_item = NSMenuItem::alloc(nil);
+            let title = make_nsstring(text);
+            msg_send![menu_item, setTitle: title];
+            msg_send![menu.menu, setTitle: title];
             menu_item.setSubmenu_(menu.menu);
             self.menu.addItem_(menu_item);
         }
     }
 
-    pub fn add_item(&mut self, id: u32, text: &str, key: impl Into<MenuKey>) {
+    pub fn add_item(&mut self, id: u32, text: &str, key: Option<&HotKey>) {
+        eprintln!("{}: {}", id, text);
         let menu_item = make_menu_item(id, text, key);
         unsafe {
             self.menu.addItem_(menu_item);
@@ -109,8 +117,78 @@ impl Default for Menu {
         let mut menu = Menu::new();
         // this one is our actual menu
         let mut submenu = Menu::new();
-        submenu.add_item(1, "Quit", 'q');
+        //submenu.add_item(1, "Quit", 'q');
         menu.add_dropdown(submenu, "Application");
         menu
+    }
+}
+impl HotKey {
+    /// Return the string value of this hotkey, for use with Cocoa `NSResponder`
+    /// objects.
+    ///
+    /// Returns the empty string if no key equivalent is known.
+    fn key_equivalent(&self) -> &str {
+        match self.key {
+            KeyCompare::Text(t) => t,
+
+            // from NSText.h
+            KeyCompare::Code(KeyCode::Return) => "\u{0003}",
+            KeyCompare::Code(KeyCode::Backspace) => "\u{0008}",
+            KeyCompare::Code(KeyCode::Delete) => "\u{007f}",
+            // from NSEvent.h
+            KeyCompare::Code(KeyCode::Insert) => "\u{F727}",
+            KeyCompare::Code(KeyCode::Home) => "\u{F729}",
+            KeyCompare::Code(KeyCode::End) => "\u{F72B}",
+            KeyCompare::Code(KeyCode::PageUp) => "\u{F72C}",
+            KeyCompare::Code(KeyCode::PageDown) => "\u{F72D}",
+            KeyCompare::Code(KeyCode::PrintScreen) => "\u{F72E}",
+            KeyCompare::Code(KeyCode::ScrollLock) => "\u{F72F}",
+            KeyCompare::Code(KeyCode::ArrowUp) => "\u{F700}",
+            KeyCompare::Code(KeyCode::ArrowDown) => "\u{F701}",
+            KeyCompare::Code(KeyCode::ArrowLeft) => "\u{F702}",
+            KeyCompare::Code(KeyCode::ArrowRight) => "\u{F703}",
+            KeyCompare::Code(KeyCode::F1) => "\u{F704}",
+            KeyCompare::Code(KeyCode::F2) => "\u{F705}",
+            KeyCompare::Code(KeyCode::F3) => "\u{F706}",
+            KeyCompare::Code(KeyCode::F4) => "\u{F707}",
+            KeyCompare::Code(KeyCode::F5) => "\u{F708}",
+            KeyCompare::Code(KeyCode::F6) => "\u{F709}",
+            KeyCompare::Code(KeyCode::F7) => "\u{F70A}",
+            KeyCompare::Code(KeyCode::F8) => "\u{F70B}",
+            KeyCompare::Code(KeyCode::F9) => "\u{F70C}",
+            KeyCompare::Code(KeyCode::F10) => "\u{F70D}",
+            //KeyCompare::Code(KeyCode::F11)            => "\u{F70E}",
+            //KeyCompare::Code(KeyCode::F12)            => "\u{F70F}",
+            //KeyCompare::Code(KeyCode::F13)            => "\u{F710}",
+            //KeyCompare::Code(KeyCode::F14)            => "\u{F711}",
+            //KeyCompare::Code(KeyCode::F15)            => "\u{F712}",
+            //KeyCompare::Code(KeyCode::F16)            => "\u{F713}",
+            //KeyCompare::Code(KeyCode::F17)            => "\u{F714}",
+            //KeyCompare::Code(KeyCode::F18)            => "\u{F715}",
+            //KeyCompare::Code(KeyCode::F19)            => "\u{F716}",
+            //KeyCompare::Code(KeyCode::F20)            => "\u{F717}",
+            KeyCompare::Code(other) => {
+                eprintln!("no key equivalent for {:?}", other);
+                ""
+            }
+        }
+    }
+
+    fn key_modifier_mask(&self) -> NSEventModifierFlags {
+        let mods: KeyModifiers = self.mods.into();
+        let mut flags = NSEventModifierFlags::empty();
+        if mods.shift {
+            flags.insert(NSEventModifierFlags::NSShiftKeyMask);
+        }
+        if mods.meta {
+            flags.insert(NSEventModifierFlags::NSCommandKeyMask);
+        }
+        if mods.alt {
+            flags.insert(NSEventModifierFlags::NSAlternateKeyMask);
+        }
+        if mods.ctrl {
+            flags.insert(NSEventModifierFlags::NSControlKeyMask);
+        }
+        flags
     }
 }