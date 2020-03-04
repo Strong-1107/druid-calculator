perlindgren/druid

Make flex child alignment configurable
This was much easier than expected; the flex widget was already
doing layout in multiple passes, so it is simply a matter of
calculating the correct position on the minor axis for a given
alignment.

druid/examples/align.rs
@@ -1,0 +1,91 @@
+// Copyright 2020 The xi-editor Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+//! Demonstrates alignment of children in the flex container.
+
+use druid::widget::{
+    Alignment, Button, Checkbox, Flex, Label, ProgressBar, Slider, Stepper, Switch, TextBox,
+    WidgetExt,
+};
+use druid::{
+    AppLauncher, Color, Data, Env, Lens, LocalizedString, PlatformError, Widget, WindowDesc,
+};
+
+#[derive(Clone, Data, Lens)]
+pub struct AppState {
+    pub input_text: String,
+    pub enabled: bool,
+    volume: f64,
+}
+
+fn make_widget_row(alignment: Alignment) -> impl Widget<AppState> {
+    Flex::row()
+        .alignment(alignment)
+        .with_child(TextBox::new().lens(AppState::input_text).center(), 0.)
+        .with_child(
+            Button::new("Clear", |_ctx, data: &mut AppState, _env| {
+                data.input_text.clear();
+                data.enabled = false;
+                data.volume = 0.0;
+            })
+            .center(),
+            0.,
+        )
+        .with_child(
+            Label::new(|data: &AppState, _: &Env| data.input_text.clone()).center(),
+            0.,
+        )
+        .with_child(Checkbox::new().lens(AppState::enabled).center(), 0.)
+        .with_child(Slider::new().lens(AppState::volume).center(), 0.)
+        .with_child(ProgressBar::new().lens(AppState::volume).center(), 0.)
+        .with_child(
+            Stepper::new()
+                .min(0.0)
+                .max(1.0)
+                .step(0.1)
+                .lens(AppState::volume),
+            0.0,
+        )
+        .with_child(Switch::new().lens(AppState::enabled), 0.)
+        .background(Color::rgba8(0, 0, 0xFF, 0x40))
+        .padding((0., 5.0))
+}
+
+fn make_ui() -> impl Widget<AppState> {
+    Flex::column()
+        .with_child(Label::new("top aligned").padding((0., 10., 0., 0.)), 0.)
+        .with_child(make_widget_row(Alignment::First), 0.)
+        .with_child(Label::new("center aligned").padding((0., 10., 0., 0.)), 0.)
+        .with_child(make_widget_row(Alignment::Center), 0.)
+        .with_child(Label::new("bottom aligned").padding((0., 10., 0., 0.)), 0.)
+        .with_child(make_widget_row(Alignment::Last), 0.)
+        .center()
+}
+
+fn main() -> Result<(), PlatformError> {
+    let main_window = WindowDesc::new(make_ui)
+        .window_size((550., 320.00))
+        .title(LocalizedString::new("Container Alignment"));
+
+    let data = AppState {
+        input_text: "hello".into(),
+        enabled: false,
+        volume: 0.0,
+    };
+
+    AppLauncher::with_window(main_window)
+        .use_simple_logger()
+        .launch(data)?;
+    Ok(())
+}

druid/src/widget/flex.rs
@@ -24,6 +24,7 @@ use crate::{
 /// A container with either horizontal or vertical layout.
 pub struct Flex<T> {
     direction: Axis,
+    alignment: Alignment,
     children: Vec<ChildWidget<T>>,
 }
 
@@ -35,6 +36,26 @@ struct ChildWidget<T> {
 pub(crate) enum Axis {
     Horizontal,
     Vertical,
+}
+
+/// The alignment of the widgets inside of the container.
+///
+/// If a widget is smaller than the container on the minor axis, this determines
+/// where it is positioned.
+#[derive(Debug, Clone, Copy)]
+pub enum Alignment {
+    /// Top or leading.
+    ///
+    /// In a vertical container, widgets are top aligned. In a horiziontal
+    /// container, their leading edges are aligned.
+    First,
+    /// Widgets are centered in the container.
+    Center,
+    /// Bottom  or trailing.
+    ///
+    /// In a vertical container, widgets are bottom aligned. In a horiziontal
+    /// container, their trailing edges are aligned.
+    Last,
 }
 
 #[derive(Copy, Clone, Default)]
@@ -73,6 +94,7 @@ impl<T> Flex<T> {
         Flex {
             direction: Axis::Horizontal,
             children: Vec::new(),
+            alignment: Alignment::First,
         }
     }
 
@@ -83,15 +105,31 @@ impl<T> Flex<T> {
         Flex {
             direction: Axis::Vertical,
             children: Vec::new(),
+            alignment: Alignment::First,
         }
     }
 
-    /// Builder-style variant of `add_child`
+    /// Builder-style method for specifying the childrens' [`Alignment`].
+    ///
+    /// [`Alignment`]: enum.Alignment.html
+    pub fn alignment(mut self, alignment: Alignment) -> Self {
+        self.alignment = alignment;
+        self
+    }
+
+    /// Builder-style variant of `add_child`.
     ///
     /// Convenient for assembling a group of widgets in a single expression.
     pub fn with_child(mut self, child: impl Widget<T> + 'static, flex: f64) -> Self {
         self.add_child(child, flex);
         self
+    }
+
+    /// Set the childrens' [`Alignment`].
+    ///
+    /// [`Alignment`]: enum.Alignment.html
+    pub fn set_alignment(&mut self, alignment: Alignment) {
+        self.alignment = alignment;
     }
 
     /// Add a child widget.
@@ -202,7 +240,10 @@ impl<T: Data> Widget<T> for Flex<T> {
         for child in &mut self.children {
             // top-align, could do center etc. based on child height
             let rect = child.widget.layout_rect();
-            let pos: Point = self.direction.pack(major, 0.0).into();
+            let extra_minor = minor - self.direction.minor(rect.size());
+            let align_minor = self.alignment.align(extra_minor);
+            let pos: Point = self.direction.pack(major, align_minor).into();
+
             child.widget.set_layout_rect(rect.with_origin(pos));
             child_paint_rect = child_paint_rect.union(child.widget.paint_rect());
             major += self.direction.major(rect.size());
@@ -227,6 +268,19 @@ impl<T: Data> Widget<T> for Flex<T> {
     fn paint(&mut self, paint_ctx: &mut PaintCtx, data: &T, env: &Env) {
         for child in &mut self.children {
             child.widget.paint_with_offset(paint_ctx, data, env);
+        }
+    }
+}
+
+impl Alignment {
+    /// Given the difference between the size of the container and the size
+    /// of the child (on their minor axis) return the necessary offset for
+    /// this alignment.
+    fn align(self, val: f64) -> f64 {
+        match self {
+            Alignment::First => 0.0,
+            Alignment::Center => val / 2.0,
+            Alignment::Last => val,
         }
     }
 }

Strong-1107/druid-calculator

druid/examples/align.rs
@@ -1,0 +1,91 @@
+// Copyright 2020 The xi-editor Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+//! Demonstrates alignment of children in the flex container.
+
+use druid::widget::{
+    Alignment, Button, Checkbox, Flex, Label, ProgressBar, Slider, Stepper, Switch, TextBox,
+    WidgetExt,
+};
+use druid::{
+    AppLauncher, Color, Data, Env, Lens, LocalizedString, PlatformError, Widget, WindowDesc,
+};
+
+#[derive(Clone, Data, Lens)]
+pub struct AppState {
+    pub input_text: String,
+    pub enabled: bool,
+    volume: f64,
+}
+
+fn make_widget_row(alignment: Alignment) -> impl Widget<AppState> {
+    Flex::row()
+        .alignment(alignment)
+        .with_child(TextBox::new().lens(AppState::input_text).center(), 0.)
+        .with_child(
+            Button::new("Clear", |_ctx, data: &mut AppState, _env| {
+                data.input_text.clear();
+                data.enabled = false;
+                data.volume = 0.0;
+            })
+            .center(),
+            0.,
+        )
+        .with_child(
+            Label::new(|data: &AppState, _: &Env| data.input_text.clone()).center(),
+            0.,
+        )
+        .with_child(Checkbox::new().lens(AppState::enabled).center(), 0.)
+        .with_child(Slider::new().lens(AppState::volume).center(), 0.)
+        .with_child(ProgressBar::new().lens(AppState::volume).center(), 0.)
+        .with_child(
+            Stepper::new()
+                .min(0.0)
+                .max(1.0)
+                .step(0.1)
+                .lens(AppState::volume),
+            0.0,
+        )
+        .with_child(Switch::new().lens(AppState::enabled), 0.)
+        .background(Color::rgba8(0, 0, 0xFF, 0x40))
+        .padding((0., 5.0))
+}
+
+fn make_ui() -> impl Widget<AppState> {
+    Flex::column()
+        .with_child(Label::new("top aligned").padding((0., 10., 0., 0.)), 0.)
+        .with_child(make_widget_row(Alignment::Start), 0.)
+        .with_child(Label::new("center aligned").padding((0., 10., 0., 0.)), 0.)
+        .with_child(make_widget_row(Alignment::Center), 0.)
+        .with_child(Label::new("bottom aligned").padding((0., 10., 0., 0.)), 0.)
+        .with_child(make_widget_row(Alignment::End), 0.)
+        .center()
+}
+
+fn main() -> Result<(), PlatformError> {
+    let main_window = WindowDesc::new(make_ui)
+        .window_size((550., 320.00))
+        .title(LocalizedString::new("Container Alignment"));
+
+    let data = AppState {
+        input_text: "hello".into(),
+        enabled: false,
+        volume: 0.0,
+    };
+
+    AppLauncher::with_window(main_window)
+        .use_simple_logger()
+        .launch(data)?;
+    Ok(())
+}

druid/src/widget/flex.rs
@@ -24,6 +24,7 @@ use crate::{
 /// A container with either horizontal or vertical layout.
 pub struct Flex<T> {
     direction: Axis,
+    alignment: Alignment,
     children: Vec<ChildWidget<T>>,
 }
 
@@ -35,6 +36,26 @@ struct ChildWidget<T> {
 pub(crate) enum Axis {
     Horizontal,
     Vertical,
+}
+
+/// The alignment of the widgets inside of the container.
+///
+/// If a widget is smaller than the container on the minor axis, this determines
+/// where it is positioned.
+#[derive(Debug, Clone, Copy)]
+pub enum Alignment {
+    /// Top or leading.
+    ///
+    /// In a vertical container, widgets are top aligned. In a horiziontal
+    /// container, their leading edges are aligned.
+    Start,
+    /// Widgets are centered in the container.
+    Center,
+    /// Bottom  or trailing.
+    ///
+    /// In a vertical container, widgets are bottom aligned. In a horiziontal
+    /// container, their trailing edges are aligned.
+    End,
 }
 
 #[derive(Copy, Clone, Default)]
@@ -73,6 +94,7 @@ impl<T> Flex<T> {
         Flex {
             direction: Axis::Horizontal,
             children: Vec::new(),
+            alignment: Alignment::Start,
         }
     }
 
@@ -83,15 +105,31 @@ impl<T> Flex<T> {
         Flex {
             direction: Axis::Vertical,
             children: Vec::new(),
+            alignment: Alignment::Start,
         }
     }
 
-    /// Builder-style variant of `add_child`
+    /// Builder-style method for specifying the childrens' [`Alignment`].
+    ///
+    /// [`Alignment`]: enum.Alignment.html
+    pub fn alignment(mut self, alignment: Alignment) -> Self {
+        self.alignment = alignment;
+        self
+    }
+
+    /// Builder-style variant of `add_child`.
     ///
     /// Convenient for assembling a group of widgets in a single expression.
     pub fn with_child(mut self, child: impl Widget<T> + 'static, flex: f64) -> Self {
         self.add_child(child, flex);
         self
+    }
+
+    /// Set the childrens' [`Alignment`].
+    ///
+    /// [`Alignment`]: enum.Alignment.html
+    pub fn set_alignment(&mut self, alignment: Alignment) {
+        self.alignment = alignment;
     }
 
     /// Add a child widget.
@@ -200,9 +238,11 @@ impl<T: Data> Widget<T> for Flex<T> {
         let mut major = 0.0;
         let mut child_paint_rect = Rect::ZERO;
         for child in &mut self.children {
-            // top-align, could do center etc. based on child height
             let rect = child.widget.layout_rect();
-            let pos: Point = self.direction.pack(major, 0.0).into();
+            let extra_minor = minor - self.direction.minor(rect.size());
+            let align_minor = self.alignment.align(extra_minor);
+            let pos: Point = self.direction.pack(major, align_minor).into();
+
             child.widget.set_layout_rect(rect.with_origin(pos));
             child_paint_rect = child_paint_rect.union(child.widget.paint_rect());
             major += self.direction.major(rect.size());
@@ -227,6 +267,19 @@ impl<T: Data> Widget<T> for Flex<T> {
     fn paint(&mut self, paint_ctx: &mut PaintCtx, data: &T, env: &Env) {
         for child in &mut self.children {
             child.widget.paint_with_offset(paint_ctx, data, env);
+        }
+    }
+}
+
+impl Alignment {
+    /// Given the difference between the size of the container and the size
+    /// of the child (on their minor axis) return the necessary offset for
+    /// this alignment.
+    fn align(self, val: f64) -> f64 {
+        match self {
+            Alignment::Start => 0.0,
+            Alignment::Center => val / 2.0,
+            Alignment::End => val,
         }
     }
 }