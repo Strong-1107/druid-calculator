swaitw/druid

Add community-made logo to the README and documentation.

druid-derive/src/lib.rs
@@ -15,6 +15,9 @@
 //! derive macros for druid.
 
 #![deny(clippy::trivially_copy_pass_by_ref)]
+#![doc(
+    html_logo_url = "https://raw.githubusercontent.com/linebender/druid/screenshots_add_logo/images/doc_logo.png"
+)]
 
 extern crate proc_macro;
 

druid-shell/src/lib.rs
@@ -29,6 +29,9 @@
 #![warn(broken_intra_doc_links)]
 #![allow(clippy::new_without_default)]
 #![deny(clippy::trivially_copy_pass_by_ref)]
+#![doc(
+    html_logo_url = "https://raw.githubusercontent.com/linebender/druid/screenshots_add_logo/images/doc_logo.png"
+)]
 
 // Rename `gtk_rs` back to `gtk`.
 // This allows us to use `gtk` as the feature name.

druid/src/lib.rs
@@ -134,6 +134,9 @@
 #![warn(missing_docs)]
 #![allow(clippy::new_ret_no_self, clippy::needless_doctest_main)]
 #![cfg_attr(docsrs, feature(doc_cfg))]
+#![doc(
+    html_logo_url = "https://raw.githubusercontent.com/linebender/druid/screenshots_add_logo/images/doc_logo.png"
+)]
 
 // Allows to use macros from druid_derive in this crate
 extern crate self as druid;

Strong-1107/druid-calculator

druid-derive/src/lib.rs
@@ -15,6 +15,9 @@
 //! derive macros for druid.
 
 #![deny(clippy::trivially_copy_pass_by_ref)]
+#![doc(
+    html_logo_url = "https://raw.githubusercontent.com/linebender/druid/screenshots/images/doc_logo.png"
+)]
 
 extern crate proc_macro;
 

druid-shell/src/lib.rs
@@ -29,6 +29,9 @@
 #![warn(broken_intra_doc_links)]
 #![allow(clippy::new_without_default)]
 #![deny(clippy::trivially_copy_pass_by_ref)]
+#![doc(
+    html_logo_url = "https://raw.githubusercontent.com/linebender/druid/screenshots/images/doc_logo.png"
+)]
 
 // Rename `gtk_rs` back to `gtk`.
 // This allows us to use `gtk` as the feature name.

druid/src/lib.rs
@@ -134,6 +134,9 @@
 #![warn(missing_docs)]
 #![allow(clippy::new_ret_no_self, clippy::needless_doctest_main)]
 #![cfg_attr(docsrs, feature(doc_cfg))]
+#![doc(
+    html_logo_url = "https://raw.githubusercontent.com/linebender/druid/screenshots/images/doc_logo.png"
+)]
 
 // Allows to use macros from druid_derive in this crate
 extern crate self as druid;